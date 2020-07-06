06fa61ca820b6bbfc4d397a828b9e7cc41af612a
Ashlee Kupor <aekupor@gmail.com>
2020-07-06 16:10:24 -0700

    [View Posts] Add methods in ViewHolder class.

--- app/src/main/java/com/example/parstagram/PostsAdapter.java
+++ app/src/main/java/com/example/parstagram/PostsAdapter.java
@@ -1,8 +1,11 @@
 package com.example.parstagram;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,12 +20,14 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        Post post = posts.get(position);
+        holder.bind(post);
     }
 
     @Override
@@ -32,8 +37,21 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
+        private TextView tvUsername;
+        private ImageView ivImage;
+        private TextView tvDescription;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            ivImage = itemView.findViewById(R.id.ivImage);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+        }
+
+        public void bind(Post post) {
+            tvDescription.setText(post.getDescription());
+            tvUsername.setText(post.getUser().getUsername());
+            //TODO: use glide for ivImage
         }
     }
 }
