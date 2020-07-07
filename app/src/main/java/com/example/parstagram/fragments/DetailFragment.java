81049af3216508a38ce9a88da4713196ba49f866
Ashlee Kupor <aekupor@gmail.com>
2020-07-07 09:47:10 -0700

    [Details] Add page variables to DetailFragment.

--- app/src/main/java/com/example/parstagram/fragments/DetailFragment.java
+++ app/src/main/java/com/example/parstagram/fragments/DetailFragment.java
@@ -1,6 +1,41 @@
 package com.example.parstagram.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.parstagram.R;
+
 public class DetailFragment extends Fragment {
+
+    private TextView tvUsername;
+    private TextView tvCreatedAt;
+    private ImageView ivImage;
+    private TextView tvDescription;
+
+    public DetailFragment() {
+        //required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_compose, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvUsername = view.findViewById(R.id.tvUsernameDetail);
+        tvCreatedAt = view.findViewById(R.id.tvCreatedAtDetail);
+        ivImage = view.findViewById(R.id.ivImageDetail);
+        tvDescription = view.findViewById(R.id.tvDescriptionDetail);
+    }
 }
