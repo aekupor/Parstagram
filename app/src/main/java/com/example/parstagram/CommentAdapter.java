48bdf781ad52182acae2d2b9001731a22224734f
Ashlee Kupor <aekupor@gmail.com>
2020-07-08 10:14:24 -0700

    [Comments] Create CommentAdapter.

---
+++ app/src/main/java/com/example/parstagram/CommentAdapter.java
@@ -0,0 +1,81 @@
+package com.example.parstagram;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ViewHolder> {
+
+    public static final String TAG = "CommentAdapter";
+
+    private Context context;
+    private List<Comment> comments;
+
+    public CommentAdapter(Context context, List<Comment> comments) {
+        this.context = context;
+        this.comments = comments;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_comment, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Comment comment = comments.get(position);
+        holder.bind(comment);
+    }
+
+    @Override
+    public int getItemCount() {
+        return comments.size();
+    }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        comments.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Comment> list) {
+        comments.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Post post) {
+
+        }
+
+        public void onClick(View v) {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // get the post at the position
+                Comment comment = comments.get(position);
+
+                Log.i(TAG, "post clicked: " + comment.getByUser().getUsername());
+            }
+        }
+    }
+}
