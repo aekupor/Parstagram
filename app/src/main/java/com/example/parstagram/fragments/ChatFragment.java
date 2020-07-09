1e7a035daaae5084d4c320824f3be7ded3bceb1b
Ashlee Kupor <aekupor@gmail.com>
2020-07-09 11:56:55 -0700

    [Chat] Add refreshMessages method.

--- app/src/main/java/com/example/parstagram/fragments/ChatFragment.java
+++ app/src/main/java/com/example/parstagram/fragments/ChatFragment.java
@@ -17,11 +17,14 @@ import android.widget.EditText;
 import com.example.parstagram.adapters.ChatAdapter;
 import com.example.parstagram.models.Message;
 import com.example.parstagram.R;
+import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ChatFragment extends Fragment {
 
@@ -29,6 +32,7 @@ public class ChatFragment extends Fragment {
 
     static final String USER_ID_KEY = "userId";
     static final String BODY_KEY = "body";
+    static final int MAX_CHAT_MESSAGES_TO_SHOW = 50;
 
     EditText etMessage;
     Button btSend;
@@ -94,6 +98,7 @@ public class ChatFragment extends Fragment {
                     public void done(ParseException e) {
                         if (e == null) {
                             Log.i(TAG, "chat was successful");
+                            refreshMessages();
                         } else {
                             Log.e(TAG, "Failed to save message", e);
                         }
@@ -102,6 +107,34 @@ public class ChatFragment extends Fragment {
                 etMessage.setText(null);
             }
         });
+    }
 
+    // Query messages from Parse so we can load them into the chat adapter
+    void refreshMessages() {
+        // Construct query to execute
+        ParseQuery<Message> query = ParseQuery.getQuery(Message.class);
+        // Configure limit and sort order
+        query.setLimit(MAX_CHAT_MESSAGES_TO_SHOW);
+
+        // get the latest 50 messages, order will show up newest to oldest of this group
+        query.orderByDescending("createdAt");
+        // Execute query to fetch all messages from Parse asynchronously
+        // This is equivalent to a SELECT query with SQL
+        query.findInBackground(new FindCallback<Message>() {
+            public void done(List<Message> messages, ParseException e) {
+                if (e == null) {
+                    mMessages.clear();
+                    mMessages.addAll(messages);
+                    mAdapter.notifyDataSetChanged(); // update adapter
+                    // Scroll to the bottom of the list on initial load
+                    if (mFirstLoad) {
+                        rvChat.scrollToPosition(0);
+                        mFirstLoad = false;
+                    }
+                } else {
+                    Log.e("message", "Error Loading Messages" + e);
+                }
+            }
+        });
     }
 }
