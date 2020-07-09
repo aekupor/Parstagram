877d8644bd2fb1a54cc93c97a413350536af8d8b
Ashlee Kupor <aekupor@gmail.com>
2020-07-09 10:53:01 -0700

    [Chat] Save messages.

--- app/src/main/java/com/example/parstagram/ChatActivity.java
+++ app/src/main/java/com/example/parstagram/ChatActivity.java
@@ -3,12 +3,58 @@ package com.example.parstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class ChatActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+    public static final String TAG = "ChatActivity";
+
+    static final String USER_ID_KEY = "userId";
+    static final String BODY_KEY = "body";
+
+    EditText etMessage;
+    Button btSend;
+
+    // Get the userId from the cached currentUser object
+    void startWithCurrentUser() {
+        setupMessagePosting();
+    }
+
+    // Setup button event handler which posts the entered message to Parse
+    void setupMessagePosting() {
+        // Find the text field and button
+        etMessage = (EditText) findViewById(R.id.etMessage);
+        btSend = (Button) findViewById(R.id.btSend);
+        // When send button is clicked, create message object on Parse
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = etMessage.getText().toString();
+                ParseObject message = ParseObject.create("Message");
+                message.put(USER_ID_KEY, ParseUser.getCurrentUser().getObjectId());
+                message.put(BODY_KEY, data);
+                message.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null) {
+                            Toast.makeText(ChatActivity.this, "Successfully created message on Parse",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "Failed to save message", e);
+                        }
+                    }
+                });
+                etMessage.setText(null);
+            }
+        });
     }
 }
