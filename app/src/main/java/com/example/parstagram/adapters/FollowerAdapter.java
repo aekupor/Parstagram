17e4b0bee6ecf7f56ca6266e71d2d8b81e83e6c0
Ashlee Kupor <aekupor@gmail.com>
2020-07-10 10:14:13 -0700

    [Followers] Update FollowersAdapter.

--- app/src/main/java/com/example/parstagram/adapters/FollowerAdapter.java
+++ app/src/main/java/com/example/parstagram/adapters/FollowerAdapter.java
@@ -2,48 +2,91 @@ package com.example.parstagram.adapters;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.parstagram.R;
+import com.example.parstagram.fragments.OtherUserProfileFragment;
 import com.example.parstagram.models.Comment;
+import com.example.parstagram.models.User;
 import com.parse.ParseException;
 
 import java.util.List;
 
-public class FollowerAdapter extends CommentAdapter {
+public class FollowAdapter extends RecyclerView.Adapter<FollowAdapter.ViewHolder> {
+
+    public static final String TAG = "FollowAdapter";
 
     private Context context;
+    private List<User> users;
 
-    public FollowerAdapter(Context context, List<Comment> comments) {
-        super(context, comments);
+    public FollowAdapter(Context context, List<User> users) {
+        this.context = context;
+        this.users = users;
     }
 
-    class CommentViewHolder extends ViewHolder {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_follower, parent, false);
+        return new ViewHolder(view);
+    }
 
-        public CommentViewHolder(@NonNull View itemView) {
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.bind(user);
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView tvUsername;
+        ImageView ivProfileImage;
+
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            tvUsername = itemView.findViewById(R.id.tvUsernameComment);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImageComment);
+
+            itemView.setOnClickListener(this);
         }
 
-        @Override
         public void bind(Comment comment) {
-            try {
-                tvUsername.setText(comment.getByUser().fetchIfNeeded().getUsername());
-            } catch (ParseException e) {
-                Log.i(TAG, e.toString());
-                e.printStackTrace();
-            }
+            //set username and profile image
+        }
 
-            if (comment.getByUser().getParseFile("profileImage") != null) {
-                Glide.with(context)
-                        .load(comment.getByUser().getParseFile("profileImage").getUrl())
-                        .into(ivProfileImage);
-            } else {
-                Glide.with(context)
-                        .load(R.drawable.ic_baseline_person_outline_24)
-                        .into(ivProfileImage);
+        public void onClick(View v) {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // make sure the position is valid, i.e. actually exists in the view
+                if (position != RecyclerView.NO_POSITION) {
+                    // get the post at the position
+                    User user = users.get(position);
+                    Log.i(TAG, "user clicked: " + user.getUsername());
+
+                    // go to OtherUserProfileFragment
+                    final FragmentManager fragmentManager = ((AppCompatActivity) context).getSupportFragmentManager();
+                    Fragment fragment = OtherUserProfileFragment.newInstance(user.getObjectId());
+                    fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                }
             }
         }
     }
