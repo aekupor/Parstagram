fb5c29f0db7c6cc1cdf6b91920f0bc663ff80085
Ashlee Kupor <aekupor@gmail.com>
2020-07-10 09:50:57 -0700

    [Followers] Create followers fragment.

---
+++ app/src/main/java/com/example/parstagram/fragments/FollowersFragment.java
@@ -0,0 +1,35 @@
+package com.example.parstagram.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.parstagram.R;
+
+public class FollowersFragment extends Fragment {
+
+    public static final String TAG = "FollowersFragment";
+
+    public FollowersFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_followers, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+}
